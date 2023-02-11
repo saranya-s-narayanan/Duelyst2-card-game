7faaee630c6165b40c1348952d2d6023cdd9b613
Saranya Sankaranarayanan <2827393s@student.gla.ac.uk>
2023-02-11 22:57:47 +0000

    2.Board creation

--- app/structures/basic/Board.java
+++ app/structures/basic/Board.java
@@ -1,6 +1,64 @@
 package structures.basic;
 
-public class Board {
-	int h=0;
+import akka.actor.ActorRef;
+import commands.BasicCommands;
+import utils.AppConstants;
+import utils.BasicObjectBuilders;
 
+
+/**
+* The board class will contain tile objects stored in a 2D array data structure and will contain 
+* methods to set the constraints of the available moves and total size of the board (9x5). 
+* The Board object consists of several tile objects.
+* 
+* */
+
+public class Board {
+
+	Tile[][] tiles = null;
+	
+	public Board(ActorRef out) {
+		tiles =new Tile[AppConstants.boardWidth][AppConstants.boardHeight];
+		setTiles(out);
+	}
+
+/**	This method creates tile objects and assign those tiles to the board object.
+ * 
+ * @param out
+ */
+	
+	public void setTiles(ActorRef out) {
+		
+		// Create a tile object
+		Tile tile;
+		
+		// Iterate through the tiles array
+		for (int i=0;i<tiles.length;i++)
+		{
+			for(int j=0;j<tiles[i].length;j++)
+			{
+				// Create a new tile object 
+				tile = BasicObjectBuilders.loadTile(i, j);
+				
+				// Assign that tile object to the array position
+				tiles[i][j]=tile;
+				
+				// Draw the tile on the front end
+				BasicCommands.drawTile(out, tile, 0);	
+
+				
+			}
+		}
+		
+	}
+	
+	
+/** Getter method to return tiles objects of a board
+ * 
+ * @return Tile
+ */
+	
+	public Tile[][] getTiles(){
+		return this.tiles;
+	}
 }
--- app/utils/AppConstants.java
+++ app/utils/AppConstants.java
@@ -1,18 +1,36 @@
 package utils;
 
+/**
+* This class file will be used to store all of the constant values that are 
+* responsible for proper game implementation. 
+*
+* This class will be also used to store any global 
+* variables that can be accessed from several Java classes. 
+*
+**/
 public class AppConstants {
 
-	public static boolean isDebugMode=true; // variable to view system logs
+	public static boolean isDebugMode=true; // variable to view system logs, set this to false to hide logs from the terminal
+	
+	public static  long gameTaskInterval= 5000; // 5 seconds
+	public static long allowedHeartbeatTimeGap = 10000; //10 seconds
 	
 	// Board constants
 	public static int boardWidth=9;
 	public static int boardHeight=5;
 	
+	
 	// Card constants
 	public static int maxCardsInHand=6;
 	public static int maxCardsInDeck=20;
 	
+	
 	// Player constants
 	public static int playerMaxHealth=20;
+	
+	public static void printLog(String message) {
+		if(isDebugMode)
+			System.out.println(message);
+	}
 	
 }
