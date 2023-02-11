345b54b3650a8fa8b80915a40107fa3b73123e20
Saranya Sankaranarayanan <2827393s@student.gla.ac.uk>
2023-02-11 22:57:47 +0000

    Board creation

--- app/structures/GameState.java
+++ app/structures/GameState.java
@@ -1,5 +1,7 @@
 package structures;
 
+import structures.basic.Board;
+
 /**
  * This class can be used to hold information about the on-going game.
  * Its created with the GameActor.
@@ -11,7 +13,33 @@ public class GameState {
 
 	
 	public boolean gameInitalised = false;
+	public boolean isGameActive = false; // Variable used for checking active front end connection
 	
-	public boolean something = false;
+	public Board board=null;
+	public long lastHeartbeatTime=0; // The time of the latest heartbeat message reception
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/** This method resets the state variable values to the default ones
+ * 
+ */
+	public void clearStateVariables() {
+		this.gameInitalised=false;
+		this.isGameActive = false;
+		this.board=null;
+		this.lastHeartbeatTime=0;
+		
+	}
 	
 }
+
+
+
