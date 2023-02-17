36ad7a0f94b70e7f7d7b7c182b61fcbea19294cb
2738037d <2738037d@student.gla.ac.uk>
2023-02-17 23:49:40 +0000

    #16 mana reset and new card draw

--- app/events/EndTurnClicked.java
+++ app/events/EndTurnClicked.java
@@ -21,11 +21,21 @@ public class EndTurnClicked implements EventProcessor{
 
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		if(gameState.isGameActive) // if the frontend connection is active
-		{
+		if(gameState.isGameActive){ // if the frontend connection is active
 			AppConstants.printLog("------> End turn Clicked:: Game is active !");
 			
-			gameState.player1.drawAnotherCard(out);
+			if(gameState.getTurn()==true){//if it was player's turn then only they can use end turn
+				gameState.player1.drawAnotherCard(out);//draw a card from the deck
+				gameState.player1.setMana(0);//flushing the mana
+				gameState.setTurn(false);//passing the turn to AI
+			}
+			else{//for when AI ends its turn
+				gameState.incrementPlayerTurn();//incrementing the turn number the player will have
+				gameState.player1.setMana(gameState.getPlayerTurnNumber()+1);//setting the mana to turn+1
+				gameState.setTurn(true);//passing the turn to the player
+			}
+			
+			
 		}
 	}
 
