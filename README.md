e50306e6bea3e98b5fd756968bc52e5f8ef6a82f
Glenn Mcfettridge <2795851m@student.gla.ac.uk>
2023-02-21 20:03:19 +0000

    resolved conflicts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MSCTeamProject
 
-This is a backend developed for a tactical card game application in Java 
+This is a backend developed for a tactical card game application in Java
 
 # Board creation
 
@@ -13,10 +13,15 @@ The constructor method has been modified to differentiate both players' attribut
 
 # Draw Card
 
-String array of card created for deck in player class corresponding to the player cards, `public void setHand
-(ActorRef out)` was created which draws 3 cards from the deck in the player hand.
+This was changed to ArrayList `Deck` and `Hand` in the player class for better handling of deck and hand.
+the `Deck` has 2 sets of card for the players and is created through the `createDeck()` method in player class, `Hand` gets 3 cards from `Deck` at initialization using the `setHand(ActorRef out, int playerID)` method. the cards are removed from the deck whenever a card is drawn using the `drawAnotherCard(ActorRef out, int playerID)` method.
 
-The end turn class was modified to include `drawAnotherCard(out)` which is defined in the player class.
+The end turn class was modified to include `drawAnotherCard(out, playerID)` to facilitate the drawing of card after endTurn is clicked.
+
+# OverDraw
+
+if the hand is full of cards, i.e from position 1-6 then the 7th position will not be drawn on the board, the
+corresponding card will be deleted from the deck. The method is part of the `drawAnotherCard(out, playerID)` in the player class.
 
 
 # Player attack ability
@@ -24,7 +29,7 @@ The end turn class was modified to include `drawAnotherCard(out)` which is defin
 
 A 'PerformAction' class has been introduced to include the methods for the implementation of available
 actions (highlight/move/attack) and perfrom appropriate tasks in order
-	
+
 	- Added highlightTiles() to highlight or unhighlight the available tiles to move or attack upon clicking
 	on avatar.
 	
@@ -32,7 +37,7 @@ actions (highlight/move/attack) and perfrom appropriate tasks in order
 
 # Get tiles containing units
 
-`getTilesWithUnits(ActorRef,Tile[][], Player)` is a method in the board class that takes in the 2d array of 
+`getTilesWithUnits(ActorRef,Tile[][], Player)` is a method in the board class that takes in the 2d array of
 tiles(board) and player and returns an ArrayList of tiles that contain a unit belonging to the input player.
 
 # Card Clicked Highlighting
@@ -45,22 +50,30 @@ method to highlight all the available summoning tiles for the given player.
 # BetterUnit creation
 
 I have modified the constructor so the board is passed into it. This is so the `addUnitToBoard(tilex,tiley)` can be used in the
-creation of the avatar. The avatar needs to be treated as any other unit for the most part so needs to be accessed 
+creation of the avatar. The avatar needs to be treated as any other unit for the most part so needs to be accessed
 through the tiles in the same way.
+
 
 # Move units
 
 The `highlightAndMove(ActorRef, GameState, Tile)` method is called in the TileClicked class and is used to first highlight the
-available units moves and then on the second click (if a friendly unit), will execute the movement. If it is an enemy unit, 
+available units moves and then on the second click (if a friendly unit), will execute the movement. If it is an enemy unit,
 it will just clear the highlighting and the player will be able to click another unit again.
 
 # Linking players to units
 
-A variable `int isPlayer` is created in the Unit class to store either the value 1 or 2 which will the used to check that the 
+A variable `int isPlayer` is created in the Unit class to store either the value 1 or 2 which will the used to check that the
 isPlayer is the same as the current playerID. There may be a better way to do this but for now it gives us all the functionality
 we need to execute core game logic.
 
 # Clearing highlighting with otherClicked
 
 I have created a method `clearHighlighting(ActorRef, Board)` which is called in the otherClicked class as well as in the tileClicked class
-int places which simply reloads the tiles with the mode '0'. 
+int places which simply reloads the tiles with the mode '0'.
+
+# GameEnd
+
+A method to check if the game has ended, when the players reach 0 health or have no cards left to play, was
+created in the PerformAction class `gameEnd(ActorRef out, GameState gameState)`. the method returns a boolean
+and also sets `gameState.isGameOver`, it is being called in the Heartbeat.java to periodically check the state.
+
--- app/events/Initalize.java
+++ app/events/Initalize.java
@@ -37,11 +37,11 @@ public class Initalize implements EventProcessor {
 
         AppConstants.printLog("------> Initialize :: creating board..");
 
-		// Create a board object and assign it to the gameState board object
+        // Create a board object and assign it to the gameState board object
         gameState.board = new Board(out);
 
         AppConstants.printLog("------> Initialize :: Board created !");
-
+        
         // creating the avatar object
         Unit avatar = BasicObjectBuilders.loadUnit(StaticConfFiles.humanAvatar, 1, Unit.class);
 
@@ -62,6 +62,30 @@ public class Initalize implements EventProcessor {
         gameState.player2.setCurrentYpos(2);
         AppConstants.printLog("------> Initialize :: Player 2 created!");
 
+
+
+
+
+
+        // ******************************* TESTS ******************************************
+        // testing if the correct tiles are highlighted
+
+        // AppConstants.printLog("------> Initialize :: Test:: Red Higlight..");
+        // gameState.board.highlightTilesRed(out, gameState.board.getAdjacentTiles(out,gameState.board.returnTile(8 ,4)));
+
+        // AppConstants.printLog("------> Initialize :: Test:: Adding dummy units on  board..");
+        // gameState.board.addDummyUnitsonBoard(out);
+
+        //************************************// HUMAN PLAYER //******************************************
+
+
+        // creating the avatar object
+        Unit avatar = BasicObjectBuilders.loadUnit(StaticConfFiles.humanAvatar, 1, Unit.class);
+
+        // Change by using the returnTile method instead of creating tile objects here
+        // placing avatar on board and setting stats
+        gameState.avatar =  new BetterUnit(out,avatar, gameState.board.returnTile(1,2), gameState.board);
+
         // creating the player object and passing the avatar object to allow the players health to be set to the avatars.
         gameState.player1 = new Player(1,out,gameState.avatar,AppConstants.deck1Cards);
         gameState.player1.setCurrentXpos(1);
@@ -70,30 +94,48 @@ public class Initalize implements EventProcessor {
 
         gameState.player1Turn=true;
         AppConstants.printLog("------> Initialize :: Set player 1 as activePlayer!");
-        
-        
-        
-        // ******************************* TESTS ******************************************
-        // testing if the correct tiles are highlighted
-       
-        // AppConstants.printLog("------> Initialize :: Test:: Red Higlight..");
-        // gameState.board.highlightTilesRed(out, gameState.board.getAdjacentTiles(out,gameState.board.returnTile(8 ,4)));
-        
-        // AppConstants.printLog("------> Initialize :: Test:: Adding dummy units on  board..");
-		// gameState.board.addDummyUnitsonBoard(out);
 
-		//***************************************************************************************
-		
-		
         //print message to the terminal notifying the start of the draw card method
-        AppConstants.printLog("------> Initialize :: Drawing 3 cards from the deck");
-        //calling the setHand method
-        gameState.player1.setHand(out);
+        AppConstants.printLog("------> Initialize :: creating deck for player 1");
+        //setting the deck as an ArrayList
+        gameState.player1.createDeck();
+        AppConstants.printLog("------> Initialize :: deck created");
+
+        AppConstants.printLog("------> Initialize :: Drawing 3 cards from the deck for player 1");
+        //Setting the hand as an ArrayList
+        gameState.player1.setHand(out,1);
         AppConstants.printLog("------> Initialize :: Card draw complete");
 
         gameState.board.addDummyUnitsonBoard(out);
 
 
+
+        //************************************// COMPUTER PLAYER //******************************************
+
+        // creating ai avatar object
+        Unit aiAvatar = BasicObjectBuilders.loadUnit(StaticConfFiles.aiAvatar, 2, Unit.class);
+
+        // Change by using the returnTile method instead of creating tile objects here
+        // placing avatar on board and setting stats
+        gameState.aiAvatar = new BetterUnit(out, aiAvatar, gameState.board.returnTile(7,2), gameState.board);
+
+        // creating the player object and passing the avatar object to allow the players health to be set to the avatars.
+        gameState.player2 = new ComputerPlayer(2,out, gameState.aiAvatar,AppConstants.deck2Cards);
+        gameState.player2.setCurrentXpos(7);
+        gameState.player2.setCurrentYpos(2);
+        AppConstants.printLog("------> Initialize :: Player 2 created!");
+
+
+        //print message to the terminal notifying the start of the draw card method
+        AppConstants.printLog("------> Initialize :: creating deck for AI");
+        //setting the deck as an ArrayList
+        gameState.player2.createDeck();
+        AppConstants.printLog("------> Initialize :: deck created");
+
+        AppConstants.printLog("------> Initialize :: Drawing 3 cards from the deck for AI");
+        //Setting the hand as an ArrayList
+        gameState.player2.setHand(out,2);
+        AppConstants.printLog("------> Initialize :: Card draw complete");
         // User 1 makes a change
         //CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
         //CheckMoveLogic.executeDemo(out);
