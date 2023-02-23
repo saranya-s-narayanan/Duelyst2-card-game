96ceb80fe0a0b2180771e4a012e4b1bf52a62b6c
Glenn Mcfettridge <2795851m@student.gla.ac.uk>
2023-02-23 03:24:11 +0000

    Added all loaded cards to an array and a method to draw them to the front-end

--- app/structures/basic/Player.java
+++ app/structures/basic/Player.java
@@ -126,6 +126,11 @@ public class Player {
 	public void syncHealth() {
 		this.health = this.avatar.getHealth();
 	}
+
+	//method to get card by hand position
+	public Card getCardByHandPos(int pos) {
+        return hand.get(pos);
+    }
 	
 	/** Setting the player health on the front end
 	 * 
@@ -185,13 +190,13 @@ public class Player {
 			Card card = BasicObjectBuilders.loadCard(cardsFiles[j], cardID, Card.class);
 			cardID++;
 			deck.add(j, card);
-			AppConstants.printLog("Card " + deck.get(j).getCardname() + " added to deck" + "at position "+ j);
+			// AppConstants.printLog("Card " + deck.get(j).getCardname() + " added to deck" + "at position "+ j);
 		}
 		for(int j=0;j<cardsFiles.length;j++){
 			Card card = BasicObjectBuilders.loadCard(cardsFiles[j], cardID, Card.class);
 			cardID++;
 			deck.add((10+j), card);
-			AppConstants.printLog("Card " + deck.get((10+j)).getCardname() + " added to deck"+ "at position "+ (10+j));
+			// AppConstants.printLog("Card " + deck.get((10+j)).getCardname() + " added to deck"+ "at position "+ (10+j));
 		}
 	}
 
@@ -213,9 +218,9 @@ public class Player {
         for(int i=0;i<AppConstants.minCardsInHand;i++){
 			//move the top card from deck to hand
 			hand.add(i, deck.get(0));
-			System.out.println("Card " + deck.get(0).getCardname() + " removing from deck");
+			// System.out.println("Card " + deck.get(0).getCardname() + " removing from deck");
 			deck.remove(0);
-			System.out.println("Card " + hand.get(i).getCardname() + " added to hand");
+			// System.out.println("Card " + hand.get(i).getCardname() + " added to hand");
 			if(playerID==1){
 				// drawCard [i]
 				BasicCommands.drawCard(out, hand.get(i), position, 0);
@@ -249,22 +254,22 @@ public class Player {
 			
 		}
 		else {
-			AppConstants.printLog("------> drawAnotherCard P1:: but the hand positions are full !, deck size: "+deck.size());
+			// AppConstants.printLog("------> drawAnotherCard P1:: but the hand positions are full !, deck size: "+deck.size());
 			if(deck.size()>0)
 			{
 				if(playerID==1){
 					BasicCommands.addPlayer1Notification(out, "Hand positions are full", 2);
-					AppConstants.printLog("------> drawAnotherCard P1:: card to be burned at position: "+ position);
+					// AppConstants.printLog("------> drawAnotherCard P1:: card to be burned at position: "+ position);
 					//deck.remove(position); //--> was creating outOfbound exception
 					deck.remove(0);
-					AppConstants.printLog("------> drawAnotherCard P1:: card burn complted!");
+					// AppConstants.printLog("------> drawAnotherCard P1:: card burn complted!");
 					AppConstants.callSleep(500);
 				}
 				else{
-					AppConstants.printLog("------> drawAnotherCard AI:: card to be burn at position: "+ position);
+					// AppConstants.printLog("------> drawAnotherCard AI:: card to be burn at position: "+ position);
 					//deck.remove(position);
 					deck.remove(0);
-					AppConstants.printLog("------> drawAnotherCard AI:: card burn complted!");
+					// AppConstants.printLog("------> drawAnotherCard AI:: card burn complted!");
 					AppConstants.callSleep(500);
 				}
 			}else {
@@ -291,7 +296,114 @@ public class Player {
 		else
 			return false;
 	}
-	
+
+	// Two lists to store the Loaded units and set their health and attack
+	public List<Unit> createPlayer1Units(ActorRef out) {
+		List <Unit> player1Units = new ArrayList<Unit>();
+
+		Unit comodoCharger = BasicObjectBuilders.loadUnit(StaticConfFiles.u_comodo_charger, 0, Unit.class);
+		comodoCharger.setIsPlayer(1);
+		comodoCharger.setHealth(3);
+		comodoCharger.setAttack(1);
+		player1Units.add(comodoCharger);
+		Unit pureBladeEnforcer = BasicObjectBuilders.loadUnit(StaticConfFiles.u_pureblade_enforcer, 1, Unit.class);
+		pureBladeEnforcer.setIsPlayer(1);
+		pureBladeEnforcer.setHealth(4);
+		pureBladeEnforcer.setAttack(1);
+		player1Units.add(pureBladeEnforcer);
+		Unit fireSpitter = BasicObjectBuilders.loadUnit(StaticConfFiles.u_fire_spitter, 2, Unit.class);
+		fireSpitter.setIsPlayer(1);
+		fireSpitter.setHealth(2);
+		fireSpitter.setAttack(3);
+		player1Units.add(fireSpitter);
+		Unit silverguardKnight = BasicObjectBuilders.loadUnit(StaticConfFiles.u_silverguard_knight, 3, Unit.class);
+		silverguardKnight.setIsPlayer(1);
+		silverguardKnight.setHealth(5);
+		silverguardKnight.setAttack(1);
+		player1Units.add(silverguardKnight);
+		Unit azureHerald = BasicObjectBuilders.loadUnit(StaticConfFiles.u_azure_herald, 5, Unit.class);
+		azureHerald.setIsPlayer(1);
+		azureHerald.setHealth(4);
+		azureHerald.setAttack(1);
+		player1Units.add(azureHerald);
+		Unit ironcliffGuardian = BasicObjectBuilders.loadUnit(StaticConfFiles.u_ironcliff_guardian, 6, Unit.class);
+		ironcliffGuardian.setIsPlayer(1);
+		ironcliffGuardian.setHealth(10);
+		ironcliffGuardian.setAttack(3);
+		player1Units.add(ironcliffGuardian);
+		Unit azuriteLion = BasicObjectBuilders.loadUnit(StaticConfFiles.u_azurite_lion, 7, Unit.class);
+		azuriteLion.setIsPlayer(1);
+		azuriteLion.setHealth(3);
+		azuriteLion.setAttack(2);
+		player1Units.add(azuriteLion);
+		Unit hailstoneGolem = BasicObjectBuilders.loadUnit(StaticConfFiles.u_hailstone_golem, 9, Unit.class);
+		hailstoneGolem.setIsPlayer(1);
+		hailstoneGolem.setHealth(6);
+		hailstoneGolem.setAttack(4);
+		player1Units.add(hailstoneGolem);
+
+		return player1Units;
+	}
+	public List<Unit> createPlayer2Units(ActorRef out) {
+
+		List <Unit> player2Units = new ArrayList<Unit>();
+
+		Unit rockPulveriser = BasicObjectBuilders.loadUnit(StaticConfFiles.u_rock_pulveriser, 20, Unit.class);
+		rockPulveriser.setIsPlayer(2);
+		rockPulveriser.setHealth(4);
+		rockPulveriser.setAttack(1);
+		player2Units.add(rockPulveriser);
+		Unit bloodshardGolem = BasicObjectBuilders.loadUnit(StaticConfFiles.u_bloodshard_golem, 21, Unit.class);
+		bloodshardGolem.setIsPlayer(2);
+		bloodshardGolem.setHealth(3);
+		bloodshardGolem.setAttack(4);
+		player2Units.add(bloodshardGolem);
+		Unit blazeHound = BasicObjectBuilders.loadUnit(StaticConfFiles.u_blaze_hound, 23, Unit.class);
+		blazeHound.setIsPlayer(2);
+		blazeHound.setHealth(3);
+		blazeHound.setAttack(4);
+		player2Units.add(blazeHound);
+		Unit windshrike = BasicObjectBuilders.loadUnit(StaticConfFiles.u_windshrike, 24, Unit.class);
+		windshrike.setIsPlayer(2);
+		windshrike.setHealth(3);
+		windshrike.setAttack(4);
+		player2Units.add(windshrike);
+		Unit pyromancer = BasicObjectBuilders.loadUnit(StaticConfFiles.u_pyromancer, 25, Unit.class);
+		pyromancer.setIsPlayer(2);
+		pyromancer.setHealth(1);
+		pyromancer.setAttack(2);
+		player2Units.add(pyromancer);
+		Unit serpenti = BasicObjectBuilders.loadUnit(StaticConfFiles.u_serpenti, 26, Unit.class);
+		serpenti.setIsPlayer(2);
+		serpenti.setHealth(4);
+		serpenti.setAttack(7);
+		player2Units.add(serpenti);
+		Unit planarScout = BasicObjectBuilders.loadUnit(StaticConfFiles.u_planar_scout, 28, Unit.class);
+		planarScout.setIsPlayer(2);
+		planarScout.setHealth(1);
+		planarScout.setAttack(2);
+		player2Units.add(planarScout);
+		Unit hailstoneGolemR = BasicObjectBuilders.loadUnit(StaticConfFiles.u_hailstone_golemR, 29, Unit.class);
+		hailstoneGolemR.setIsPlayer(2);
+		hailstoneGolemR.setHealth(6);
+		hailstoneGolemR.setAttack(4);
+		player2Units.add(hailstoneGolemR);
+
+		return player2Units;
+
+	}
+
+	// method to draw the unit to the board and set the front end attack and health
+	public void drawUnitToBoard(ActorRef out, Unit unit,Tile tile) {
+
+		BasicCommands.drawUnit(out, unit, tile);
+		AppConstants.callSleep(100);
+		BasicCommands.setUnitHealth(out, unit, unit.getHealth());
+		AppConstants.callSleep(100);
+		BasicCommands.setUnitAttack(out, unit, unit.getAttack());
+		AppConstants.callSleep(100);
+
+	}
 	
 	
 }
