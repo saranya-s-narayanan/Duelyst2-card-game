62315a613c4436e1390a05f978e4386f4bbf283d
Mona <2829922e@student.gla.ac.uk>
2023-03-03 06:01:20 +0000

    Mana Increase Test - failed

--- test/ManaTest.java
+++ test/ManaTest.java
@@ -15,10 +15,10 @@ import structures.basic.Board;
 import structures.basic.Player;
 import utils.AppConstants;
 
-public class AIAvatarNotNullTest {
+public class ManaTest {
 
 	@Test
-	public void AIAvatarNotNullTest() {
+	public void ManaTest() {
 
 		CheckMessageIsNotNullOnTell altTell = new CheckMessageIsNotNullOnTell(); // create an alternative tell
 		BasicCommands.altTell = altTell; // specify that the alternative tell should be used
@@ -30,9 +30,17 @@ public class AIAvatarNotNullTest {
 
 		ObjectNode eventmessage = Json.newObject();
 		initializeProcessor.processEvent(null, gameState, eventmessage);
-
-		// Check if AI Avatar is not null when game is initialized)
-		assertNotNull("Player should be initialized", gameState.aiAvatar);
+		
+		//Initializing a dummy player and dummy AI player with maxHealth and playerTurnNumber/compTurnNumber from gameState
+		Player x = new Player(AppConstants.playerMaxHealth, gameState.playerTurnNumber);
+		Player y = new Player(AppConstants.playerMaxHealth, gameState.compTurnNumber);
+		
+		//Checks that getMana equals the turn number + 1
+		assertTrue(x.getMana() == gameState.playerTurnNumber+1);
+		assertTrue(y.getMana() == gameState.compTurnNumber+1);
+		
+		//This test is failing - got this error: Test ManaTest.ManaTest failed: java.lang.AssertionError: null, took 5.529 sec
+		//https://community.pivotal.io/s/question/0D50e00006jq8eCCAQ/what-causes-javalangassertionerror-null-at-orgapachecatalinamappermapperinternalmapmapperjava744?language=en_US
 
 	}
 
