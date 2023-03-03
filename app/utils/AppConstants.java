aed6b165164f9d41c0655897c1d9b1831d912fca
2738037d <2738037d@student.gla.ac.uk>
2023-03-03 21:15:23 +0000

    #69 appconstant changed to remove card config

--- app/utils/AppConstants.java
+++ app/utils/AppConstants.java
@@ -42,46 +42,15 @@ public class AppConstants {
 	public static String drawCard="drawCard";
 
 
-
-	
-	//string array of player 1 deck
-	public static String[] deck1Cards = {
-		StaticConfFiles.c_comodo_charger,
-		StaticConfFiles.c_pureblade_enforcer,
-		StaticConfFiles.c_fire_spitter,
-		StaticConfFiles.c_silverguard_knight,
-		StaticConfFiles.c_truestrike,
-		StaticConfFiles.c_azure_herald,
-		StaticConfFiles.c_ironcliff_guardian,
-		StaticConfFiles.c_azurite_lion,
-		StaticConfFiles.c_sundrop_elixir,
-		StaticConfFiles.c_hailstone_golem
-	};
-		
-	public static String[] deck2Cards = {
-		StaticConfFiles.c_rock_pulveriser,
-		StaticConfFiles.c_bloodshard_golem,
-		StaticConfFiles.c_staff_of_ykir,
-		StaticConfFiles.c_blaze_hound,
-		StaticConfFiles.c_windshrike,
-		StaticConfFiles.c_pyromancer,
-		StaticConfFiles.c_serpenti,
-		StaticConfFiles.c_entropic_decay,
-		StaticConfFiles.c_planar_scout,
-		StaticConfFiles.c_hailstone_golem
-	};
-
 	//string array of player 1 units
 	public static String[] p1unit = {
 		StaticConfFiles.u_comodo_charger,
 		StaticConfFiles.u_pureblade_enforcer,
 		StaticConfFiles.u_fire_spitter,
 		StaticConfFiles.u_silverguard_knight,
-		// StaticConfFiles.u_truestrike,
 		StaticConfFiles.u_azure_herald,
 		StaticConfFiles.u_ironcliff_guardian,
 		StaticConfFiles.u_azurite_lion,
-		// StaticConfFiles.u_sundrop_elixir,
 		StaticConfFiles.u_hailstone_golem
 	};
 
@@ -89,12 +58,10 @@ public class AppConstants {
 	public static String[] p2unit = {
 		StaticConfFiles.u_rock_pulveriser,
 		StaticConfFiles.u_bloodshard_golem,
-		// StaticConfFiles.u_staff_of_ykir,
 		StaticConfFiles.u_blaze_hound,
 		StaticConfFiles.u_windshrike,
 		StaticConfFiles.u_pyromancer,
 		StaticConfFiles.u_serpenti,
-		// StaticConfFiles.u_entropic_decay,
 		StaticConfFiles.u_planar_scout,
 		StaticConfFiles.u_hailstone_golem
 	};
