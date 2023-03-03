7a90d872e15527b0a55d8eed92a10c26a5eb8ec7
Mona <2829922e@student.gla.ac.uk>
2023-03-03 06:48:20 +0000

    Mana Test - passed

--- test/ManaTest.java
+++ test/ManaTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import akka.actor.ActorRef;
 import commands.BasicCommands;
 import commands.CheckMessageIsNotNullOnTell;
+import events.EndTurnClicked;
+import events.EventProcessor;
 import events.Initalize;
 import play.libs.Json;
 import structures.GameState;
@@ -28,20 +31,27 @@ public class ManaTest {
 
 		assertFalse(gameState.gameInitalised); // check we have not initalized
 
+		//This sets up the GameState and initializes the players. See Initialize.java to confirm what is instantiated
 		ObjectNode eventmessage = Json.newObject();
 		initializeProcessor.processEvent(null, gameState, eventmessage);
+				
+		// Creating a new EndTurnClicked object to get to processEvent, which has the logic for switching the player turns
+		EndTurnClicked e = new EndTurnClicked();
 		
-		//Initializing a dummy player and dummy AI player with maxHealth and playerTurnNumber/compTurnNumber from gameState
-		Player x = new Player(AppConstants.playerMaxHealth, gameState.playerTurnNumber);
-		Player y = new Player(AppConstants.playerMaxHealth, gameState.compTurnNumber);
+		//Ends player1's turn 
+		e.processEvent(null, gameState, eventmessage);
 		
-		//Checks that getMana equals the turn number + 1
-		assertTrue(x.getMana() == gameState.playerTurnNumber+1);
-		assertTrue(y.getMana() == gameState.compTurnNumber+1);
+		//Checks that human player's getMana equals the turn number + 1 as the turn changes 
+		assertTrue(gameState.player1.getMana() == gameState.playerTurnNumber+1);
 		
-		//This test is failing - got this error: Test ManaTest.ManaTest failed: java.lang.AssertionError: null, took 5.529 sec
-		//https://community.pivotal.io/s/question/0D50e00006jq8eCCAQ/what-causes-javalangassertionerror-null-at-orgapachecatalinamappermapperinternalmapmapperjava744?language=en_US
-
+		//Ends player2's turn 
+		e.processEvent(null, gameState, eventmessage);
+		
+		//Checks that AI player's getMana equals the turn number + 1 as the turn changes 
+		assertTrue(gameState.player2.getMana()== gameState.compTurnNumber+1);
+		
+		//**Need to add this test and IsGameOver to the README**
+		
 	}
 
 }
