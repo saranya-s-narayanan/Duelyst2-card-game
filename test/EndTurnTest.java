7f12977763e693aa38f68d9c6934dbe68baa8eab
Mona <2829922e@student.gla.ac.uk>
2023-03-05 23:13:25 +0000

    EndTurnTest revised

--- test/EndTurnTest.java
+++ test/EndTurnTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import akka.actor.ActorRef;
@@ -18,9 +19,6 @@ import structures.basic.Board;
 import structures.basic.Player;
 import utils.AppConstants;
 
-import akka.actor.ActorRef;
-//This should allow me to get the ActorRef done
-
 public class EndTurnTest {
 
 	@Test
@@ -32,43 +30,18 @@ public class EndTurnTest {
 		GameState gameState = new GameState();
 		Initalize initializeProcessor = new Initalize();
 
-//		assertFalse(gameState.gameInitalised); // check we have not initalized
-
-		//This sets up the GameState and initializes the players. See Initialize.java to confirm what is instantiated
+		// This sets up the GameState and initializes the players. See Initialize.java
+		// to confirm what is instantiated
 		ObjectNode eventmessage = Json.newObject();
 		initializeProcessor.processEvent(null, gameState, eventmessage);
-				
-		// Creating a new EndTurnClicked object to get to processEvent, which has the logic for switching the player turns
-		EndTurnClicked e = new EndTurnClicked();
-		
-		ActorRef out;
-				
-		//Ends player1's turn 
-		e.processEvent(out, gameState, eventmessage);
-		
-		//Checks that player1's turn ends once this event is processed
-		assertFalse(gameState.player1Turn);
-		
-		//Ends player2's turn 
-		e.processEvent(out, gameState, eventmessage);
-		
-		//Checks that player2's turn ends once this event is processed
-		assertTrue(gameState.player1Turn);
 
-		//**Need to add this test and IsGameOver to the README**
-		
-		//Received the error messages below for this test
-		
-//		[error] Test ManaTest.ManaTest failed: java.lang.AssertionError: null, took 5.597 sec
-//		[error]     at ManaTest.ManaTest(ManaTest.java:45)
-//		[error]     at jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
-//		[error]     at jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
-//		[error]     at jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
-//		------> message type:---->null
-//		------> Initialize :: creating board..
-//		[error]     at java.lang.reflect.Method.invoke(Method.java:566)
-//		[error]     ...
-		
+		//Confirms that when player1Turn is set to true, it is player1's turn
+		///Else if it is false, it is player2's turn
+		if (gameState.player1Turn == true) {
+			assertTrue(gameState.player1Turn);
+		} else {
+			assertFalse(gameState.player1Turn);
+		}
 	}
 
 }
